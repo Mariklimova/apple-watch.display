3442075628c37d58018ca09207cbcc6d2f538840
Mariklimova <mariku22@yandex.ru>
2023-10-03 01:28:10 +0300

    project extension

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 
 
-function test() {
+function getTime() {
     const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    const year = date.getFullYear();
-    document.querySelector('.year p') = year;
+    let hours = date.getHours().toString();
+    let minutes = date.getMinutes().toString();
+    let seconds = date.getSeconds().toString();
+    let month = date.getMonth() + 1;
+    let day = date.getDate().toString();
+    const year = date.getFullYear().toString();
+
+
+
+    hours.length < 2 ? hours = '0' + hours : hours;
+    minutes.length < 2 ? minutes = '0' + minutes : minutes;
+    seconds.length < 2 ? seconds = '0' + seconds : seconds;
+    // month.length < 2 ? month = '0' + month : month;
+    day.length < 2 ? day = '0' + day : day;
+
+
+    document.querySelector('.year p').textContent = day + ' ' + month + ' ' + year;
+    document.querySelector('.time').textContent = hours + ':' + minutes + ':' + seconds;
 }
-setInterval(test, 1000)
+setInterval(getTime, 1000)
